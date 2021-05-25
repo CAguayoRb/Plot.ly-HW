8de29f6b038aa158e7575add1d83acfb4654aa83
Cecilia Aguayo <78163790+CAguayoRb@users.noreply.github.com>
2021-05-24 20:00:36 -0700

    tryingtofixit

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -1,35 +1,35 @@
-function buildCharts(id) {
+// Get data to build plots 
+function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
-
+      var otus_data = data.metadata.map (d => d.otus_data)
       var otu_ids = data.samples[0].otu_ids;
-      var sample_values =  data.samples.sample_values.slice(0,10).reverse();
+      var sample_values = data.samples.sample_values.slice(0,10).reverse();
       var otu_labels =  data.samples.otu_labels.slice(0,10);
       var topOTU = (data.samples.otu_ids.slice(0, 10)).reverse();
       var OTUS = topOTU.map(d => "OTU " + d);
       var samples = data.samples.filter(s => s.id.toString() === id);
 
-      var trace = {
+      var dataBar = {
+          type:"bar",
           x: sampleValues,
           y: OTUS,
           text: otu_labels,
-          type:"bar",
-          orientation: "h",
           marker: {
           color: '#0000FF'},
       };
 
-      var data = [trace];
 
-      var layout = {
+      var layoutBar = {
           title: "Top 10 Bacteria Cultures Found:",
           height: 450,
           width: 600
       }
 
-      Plotly.newPlot("bar", data, layout);
+      Plotly.newPlot("bar", dataBar, layoutBar);
       
+      buildPlot();
   
-      var trace_2 = {
+      var dataBubble = {
           x: samples.otu_ids,
           y: samples.sample_values,
           mode: "markers",
@@ -40,63 +40,66 @@ function buildCharts(id) {
           text: data.otu_labels
       };
 
-      var layoutOTU_ID = {
+      var layoutBubble = {
           xaxis:{title: "OTU ID"},
           height: 700,
           width: 800
       };
 
-      var data_OTU = [trace1];
 
-
-      Plotly.newPlot("bubble", data_OTU, layoutOTU_ID); 
+      Plotly.newPlot("bubble", dataBubble, layoutBubble); 
   
   });
-}  
+}
 
-function demographicINFO(id) {
+buildPlot();
+
+//Get info for the demographics table. Build table. 
+
+function DemoInfo(sample) {
   d3.json("samples.json").then((data)=> {
 
       var metaData = data.metaData;
-
-      var filteredResult = metaData.filter(meta => meta.id.toString() === id)[0];
+    // var table = d3.select("#summaryTableMetadata");
+    // var tbody = table.select("tbody");
+    // buildTable = (otus_data, otu_ids, sample_values);
+      var filteredResult = metaData.filter(sampleobject => sampleobject(id))
       
-      var demographic_info = d3.select("#sample-metadata");
+      var demographic_info = d3.select("#sample-metaData");
       
       demographic_info.html("");
 
-      Object.entries(filteredResult).forEach((id) => {   
-          demographic_info.append("h5")
-          .text(id[0]
-          .toUpperCase() + ": " + id[1] + "\n");    
+      Object.entries(filteredResult).forEach((sample) => {   
+          demographic_info.append("tr")
+          .text(id)    
       });
   });
 }
 
-function options(id) {
-  buildCharts(id);
-  demographicINFO(id);
-}
+//function getNewInfo(sample) {
+//  buildCharts(sample);
+// DemoInfo(sample);
+// }
 
 
 function init() {
-  var dropdownMenu = d3.select("#selDataset");
+  var dropdownOTU = d3.select("#selDataset");
   d3.json("samples.json").then((data)=> {
       data.names.forEach(function(name) {
-          dropdownMenu.append("option")
-          .text(name)
-          .property("value");
+          dropdownOTU.append("option")
+          .text(sample)
+          .property("value", sample);
       });
       
       buildCharts(data.names[0]);
-      demographicINFO(data.names[0]);
+      DemoInfo(data.names[0]);
   });
 };
-init();
+//init();
 
-function optionChanged(id) {
-  buildCharts(id);
-  demographicINFO(id);
+function newInfo(sample) {
+  buildCharts(sample);
+  DemoInfo(sample);
 };
 
 init();
